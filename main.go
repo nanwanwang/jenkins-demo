fcaf68196b093bcad31d662fac513380f4cf014d
demon.wang <demon.wang@cloudkeeper.cn>
2019-09-10 15:59:39 +0800

    dev to master

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI！")
+	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI！from dev to master! ")
 	fmt.Println("BRANCH_NAME:", os.Getenv("branch"))
 }
